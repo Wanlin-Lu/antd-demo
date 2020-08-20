ee6487f9d2ff00da41f5b3ed8be871d3552a4006
Wanlin-Lu <luwanlin@126.com>
2020-08-20 20:21:51 +0800

    App Grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Layout } from 'antd'
+import { Row } from 'antd'
 import { CheckCircleTwoTone } from '@ant-design/icons'
+import Header from './layout-page/Header'
 import './App.less';
+import Sider from './layout-page/Sider'
+import Content from './layout-page/Content'
+import Footer from './layout-page/Footer'
 
 function App() {
   return (
-    <Layout.Header>
-      <CheckCircleTwoTone />
-    </Layout.Header>
+    <React.Fragment>
+      <Row>
+        <Header />
+      </Row>
+      <Row>
+        <Sider />
+        <Content />
+      </Row>
+      <Row>
+        <Footer />
+      </Row>
+    </React.Fragment>
   );
 }
 
