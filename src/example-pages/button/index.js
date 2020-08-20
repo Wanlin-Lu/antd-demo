8712bd90fba94a07183a127ce087537a7eebad1d
Wanlin-Lu <luwanlin@126.com>
2020-08-20 20:42:06 +0800

    buttonPage:update

--- src/example-pages/button/index.js
+++ src/example-pages/button/index.js
@@ -17,6 +17,27 @@ const ButtonPage = () => {
         <Tooltip title="search">
           <Button type="primary" shape="circle" icon={<SearchOutlined />} />
         </Tooltip>
+        <Button type="primary" shape="circle">
+          A
+        </Button>
+        <Button type="primary" icon={<SearchOutlined />}>
+          Search
+        </Button>
+        <Tooltip title="search">
+          <Button shape="circle" icon={<SearchOutlined />} />
+        </Tooltip>
+        <Button icon={<SearchOutlined />}>Search</Button>
+        <br />
+        <Tooltip title="search">
+          <Button shape="circle" icon={<SearchOutlined />} />
+        </Tooltip>
+        <Button icon={<SearchOutlined />}>Search</Button>
+        <Tooltip title="search">
+          <Button type="dashed" shape="circle" icon={<SearchOutlined />} />
+        </Tooltip>
+        <Button type="dashed" icon={<SearchOutlined />}>
+          Search
+        </Button>
       </Col>
     </React.Fragment>
   );
