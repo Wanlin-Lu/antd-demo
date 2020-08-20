bb10fe29dc24dccb05a385c21b99a3d6bd65d88f
Wanlin-Lu <luwanlin@126.com>
2020-08-20 20:22:34 +0800

    buttonPage start

--- src/example-pages/button/index.js
+++ src/example-pages/button/index.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import { Button } from 'antd'
+import { Button, Col, Tooltip } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
 
 const ButtonPage = () => {
   return (
     <React.Fragment>
-      <Button type="primary">Primary Button</Button>
-      <Button>Default Button</Button>
-      <Button type="dashed">Dashed Button</Button>
-      <br />
-      <Button type="text">Text Button</Button>
-      <Button type="link">Link Button</Button>
+      <Col>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <br />
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+      </Col>
+      <Col>
+        <Tooltip title="search">
+          <Button type="primary" shape="circle" icon={<SearchOutlined />} />
+        </Tooltip>
+      </Col>
     </React.Fragment>
-  )
+  );
 }
 
 export default ButtonPage
