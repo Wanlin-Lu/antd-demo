02a0e1c5d969cdb55a27196f446c9fa44aa24d2b
Wanlin-Lu <luwanlin@126.com>
2020-08-20 20:19:12 +0800

    layout:Header

--- src/layout-page/Header/index.js
+++ src/layout-page/Header/index.js
@@ -1,23 +1,32 @@
 import React from 'react'
-import { Layout, Button } from 'antd'
-import { SmileTwoTone, DownloadOutlined } from "@ant-design/icons";
+import { Layout, Button, Col } from 'antd'
+import { SmileTwoTone } from "@ant-design/icons";
 
 import './style.css'
 
 const Header = () => {
   return (
-    <Layout.Header>
-      <div className="logo">
-        <SmileTwoTone className="logo-icon" twoToneColor="#226622" />
-        Antd
-      </div>
-      <div className="auth">
-        <span className="username">Wanlin-Lu</span>
-        <Button type="primary" shape="round" size="small" className="auth-button">
-          Logout
-        </Button>
-      </div>
-    </Layout.Header>
+    <Col
+      span={24}
+    >
+      <Layout.Header>
+        <div className="logo">
+          <SmileTwoTone className="logo-icon" twoToneColor="#226622" />
+          Antd
+        </div>
+        <div className="auth">
+          <span className="username">Wanlin-Lu</span>
+          <Button
+            type="primary"
+            shape="round"
+            size="small"
+            className="auth-button"
+          >
+            Logout
+          </Button>
+        </div>
+      </Layout.Header>
+    </Col>
   );
 }
 
