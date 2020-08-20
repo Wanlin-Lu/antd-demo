26b47219dfb3e2fe688a08dfa2451743dbfdf5e6
Wanlin-Lu <luwanlin@126.com>
2020-08-20 20:19:47 +0800

    layout:Content

--- src/layout-page/Content/index.js
+++ src/layout-page/Content/index.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Row, Col } from 'antd'
 import { PlusCircleOutlined } from '@ant-design/icons'
 
 import ButtonPage from '../../example-pages/button'
+import IconsPage from '../../example-pages/icons'
 
 
 const Content = () => {
   return (
-    <Layout.Content>
-      
-      <ButtonPage />
-      <div>
-        <Layout.Header />
-      </div>
+    <Layout.Content style={{ padding: "18px 15px" }}>
+      <Row gutter={[16, 16]} justify="space-around">
+        <ButtonPage />
+      </Row>
+      <Row gutter={[16, 16]} justify="center">
+        <IconsPage />
+      </Row>
     </Layout.Content>
-  )
+  );
 }
 
 export default Content
